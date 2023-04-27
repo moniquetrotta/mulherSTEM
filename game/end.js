e2bf3e9ff88f44467d4b0e11e09637e3ad1991e3
moniquetrotta <monique.trotta@gmail.com>
2023-04-27 11:27:34 -0300

    acertando botoes

--- game/end.js
+++ game/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = localStorage.getItem('#mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 9;
@@ -12,6 +12,7 @@ username.addEventListener('keyup', () => {
   saveScoreBtn.disabled = !username.value;
 });
 
+let saveHighScore;
 saveHighScore = (e) => {
   e.preventDefault();
 
@@ -26,5 +27,5 @@ saveHighScore = (e) => {
   highScores.splice(9);
 
   localStorage.setItem('highScores', JSON.stringify(highScores));
-  window.location.assign('/');
+  window.location.assign('/game/index.html');
 };
